2207d01cd84b35e7bb5c57f8dd8c23b5f0868120
oduoranto <dev.antonyoduor@gmail.com>
2024-07-03 08:55:21 +0300

    added database

--- global/settings.py
+++ global/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'global.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
