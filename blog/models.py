70350b2b2c88b66ee46a7bb9de4425464ab79079
oduoranto <dev.antonyoduor@gmail.com>
2024-07-03 21:17:45 +0300

    edited models.py file in blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class PostModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey()
+
+
