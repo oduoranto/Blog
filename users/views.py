5ab9aedaca2d1b57cf8d20cb39ba559341b43152
oduoranto <dev.antonyoduor@gmail.com>
2024-07-04 17:08:37 +0300

    added views in users/views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def sign_up(request):
+    return render(request, 'users/sign_up.html')
