56a102f113d8c32cdceb2cae4d7c3805332755d8
oduoranto <dev.antonyoduor@gmail.com>
2024-07-04 10:47:17 +0300

    created a file forms.py

--- blog/models.py
+++ blog/models.py
@@ -9,4 +9,10 @@ class PostModel(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ('-date_created',)
+
+    def __str__(self):
+        return self.title
+
 
