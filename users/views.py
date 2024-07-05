586b8455fe9a70f41da2fed7674c0d2e5cc9038a
oduoranto <dev.antonyoduor@gmail.com>
2024-07-05 10:29:53 +0300

    "Edited"

--- users/views.py
+++ users/views.py
@@ -17,3 +17,6 @@ def sign_up(request):
         'form' : form,
     }
     return render(request, 'users/sign_up.html', context)
+
+def profile(request):
+   return render(request, 'users/profile.html')
