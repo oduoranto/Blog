d4b5b38f8b5dc208e21520fca584e043b28ecad2
oduoranto <dev.antonyoduor@gmail.com>
2024-07-08 23:31:43 +0300

    "changes"

--- users/forms.py
+++ users/forms.py
@@ -23,8 +23,17 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 
+    def __init__(self, *args, **kwargs):
+        super(UserUpdateForm, self).__init__(*args, **kwargs)
+        for fieldname in ['username','email']:
+            self.fields[fieldname].help_text = None
+ 
+
 class ProfileUpdateForm(forms.ModelForm):  
     class Meta:
         model = ProfileModel
-        fields = ['image']      
+        fields = ['image']     
+    
+        
+        
 
