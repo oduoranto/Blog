f39f172692d98f824e2c1c6687df7ea58441667c
oduoranto <dev.antonyoduor@gmail.com>
2024-07-08 23:41:56 +0300

    "Changes"

--- users/views.py
+++ users/views.py
@@ -26,6 +26,7 @@ def profile(request):
       if u_form.is_valid() and p_form.is_valid():
          u_form.save()
          p_form.save()
+         return redirect('users-profile')
    else:   
       u_form = UserUpdateForm(instance=request.user)
       p_form = ProfileUpdateForm(instance=request.user.profilemodel)
