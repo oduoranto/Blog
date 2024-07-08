b0e0ca129e870787590b385271a323940c67b389
oduoranto <dev.antonyoduor@gmail.com>
2024-07-08 23:17:27 +0300

    "Changes on profile"

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from typing import Any
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import  ProfileModel
 from django import forms
 
 class SignUpForm(UserCreationForm):
@@ -15,3 +16,15 @@ class SignUpForm(UserCreationForm):
         
         for fieldname in ['username','email','password1','password2']:
             self.fields[fieldname].help_text = None
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):  
+    class Meta:
+        model = ProfileModel
+        fields = ['image']      
+
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 
@@ -19,5 +19,18 @@ def sign_up(request):
     return render(request, 'users/sign_up.html', context)
 
 def profile(request):
-   return render(request, 'users/profile.html')
+   if request.method == 'POST':
+      u_form = UserUpdateForm(request.POST)
+      p_form = ProfileUpdateForm(request.POST)
+   else:   
+      u_form = UserUpdateForm()
+      p_form = ProfileUpdateForm()
+
+   context = {
+       'u_form': u_form,
+       'p_form': p_form
+
+       
+    }  
+   return render(request, 'users/profile.html', context)
 
