74c57a4e7fd202695442052382b947c3d6e7dac7
oduoranto <dev.antonyoduor@gmail.com>
2024-07-10 16:21:36 +0300

    " "

--- blog/forms.py
+++ blog/forms.py
@@ -13,7 +13,7 @@ class PostUpdateForm(forms.ModelForm):
         fields = ('title', 'content')        
 
 class CommentForm(forms.ModelForm):
-    content = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add comment here'}))
+    content = forms.CharField(label='',widget=forms.TextInput(attrs={'placeholder': 'Add comment here...'}))
     class Meta:
         model = Comment
         fields =('content',)
