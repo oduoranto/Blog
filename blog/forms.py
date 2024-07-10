5655b35f1443cb500532839254fb252add0e608c
oduoranto <dev.antonyoduor@gmail.com>
2024-07-10 16:04:39 +0300

    " "

--- blog/forms.py
+++ blog/forms.py
@@ -13,6 +13,7 @@ class PostUpdateForm(forms.ModelForm):
         fields = ('title', 'content')        
 
 class CommentForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'rows': 4}))
     class Meta:
         model = Comment
         fields =('content',)
