25a9cb593f1e077c64655add36b439d278bfc0ea
oduoranto <dev.antonyoduor@gmail.com>
2024-07-10 15:27:03 +0300

    "registed comments in admin.py"

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import PostModel
+from .models import PostModel, Comments
 
 # Register your models here.
 class PostModelAdmin(admin.ModelAdmin):
     list_display= ('title', 'date_created')
 
 admin.site.register(PostModel, PostModelAdmin)
+admin.site.register(Comments)
