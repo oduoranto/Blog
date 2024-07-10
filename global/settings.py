8f86a6e8e16ace7e429ab7f3a3f41b2656822572
oduoranto <dev.antonyoduor@gmail.com>
2024-07-11 01:10:26 +0300

    "  "

--- global/settings.py
+++ global/settings.py
@@ -145,7 +145,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER ='glo⚽@gmail.com'
+EMAIL_HOST_USER ='gloBALL@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True    
