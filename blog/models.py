7c26092844c66e2431e0a07d0c0bbcd93181b35b
oduoranto <dev.antonyoduor@gmail.com>
2024-07-10 15:25:55 +0300

    "  "

--- blog/models.py
+++ blog/models.py
@@ -15,4 +15,12 @@ class PostModel(models.Model):
     def __str__(self):
         return self.title
 
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE )
+    post = models.ForeignKey(PostModel, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+
+    def __str__(self) :
+        return self.content
+
 
