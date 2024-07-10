f03d4fbfbc407d5affd710daca9fe7516efd4d0f
oduoranto <dev.antonyoduor@gmail.com>
2024-07-11 00:56:07 +0300

    " ....."

--- global/settings.py
+++ global/settings.py
@@ -141,3 +141,10 @@ STATIC_ROOT =BASE_DIR / 'asset'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER ='emailaddress'
+EMAIL_HOST_PASSWORD = 'emailpassword'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True    
