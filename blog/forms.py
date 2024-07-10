164087a91bce5779f0bae5818917e8e1fbdf143f
oduoranto <dev.antonyoduor@gmail.com>
2024-07-10 15:55:51 +0300

    " "

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import PostModel
+from .models import PostModel, Comment
 
 class PostModelForm(forms.ModelForm):
     content = forms.CharField(widget=forms.Textarea(attrs={'rows': 4}))
@@ -11,3 +11,8 @@ class PostUpdateForm(forms.ModelForm):
     class Meta:
         model = PostModel
         fields = ('title', 'content')        
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields =('content',)
